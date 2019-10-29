b3fb99010fbdd8879acc9d7e92c3a8b778595df0
CHAO CHENG LIN(Ken) <38403949+lin60102@users.noreply.github.com>
2019-10-28 23:14:26 -0400

    Ken's code

---
+++ AGD Midterm Shooter/Assets/Script/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    private static GameManager Instance; // **<- reference link to GameManager
+     
+    
+     
+     private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+     
+   
+}
